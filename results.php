0596a1267e318675b665ed05c04e7bd9a17ab492
Spoonful-Creative <pughsinead@hotmail.com>
2017-02-27 16:32:25 +1300

    Adding Data Content Supplied by Ash

--- results.php
+++ results.php
@@ -1,39 +1,27 @@
 <?php
 $searchQuery = $_GET['q'];
 
-$data = array(
-  array(
-    'text' => 'Apple',
-    'url' => 'http://apple.com'
-    ),
-  array(
-    'text' => 'Bakers delight',
-    'url' => 'http://bakersdelight.com'
-    ),
-  array(
-    'text' => 'Twitter',
-    'url' => 'http://twitter.com'
-    ),
-  );
-// die(var_dump($data));
-// foreach ($data as $term) {
-//     echo '<a href="' . $term['url'] . '">' . $term['text'] . '</a></br>';
-// }
+$data = require 'includes/data.php';
 
-foreach ($data as $term): 
-  ?>
-    <a href="<?= $term['url'] ?>"><?= $term['text'] ?></a></br>
-<?php
-endforeach;
-die();
-
+function filterResults($searchQuery, $data) {
+        $matches = [];
+ 
+        foreach ($data as $key => $value) {
+          if (strpos(strtolower($value['text']), strtolower($searchQuery)) !== false) {
+            $matches[] = $value;
+          }
+        }
+        return $matches;
+    }
+ 
+$terms = filterResults($searchQuery, $data);
 
 
 require 'partials/header.php';
 require 'partials/navigation.php';
 ?>
 
- 
+
 
 <!-- Content -->
 <div class="container">
@@ -42,52 +30,27 @@ require 'partials/navigation.php';
       <h4>Search results for <strong><?php echo $searchQuery; ?></strong></h4>
       <!-- Displayed results -->
       <ul class="list-group">
+
+        <?php
+        foreach ($terms as $term): 
+          ?>
+
         <!-- Single Result -->
         <li class="list-group-item notification-bar-fail m-b-1">
-          <div href="#" class="notification-bar-icon">
-            <div>
-              <i></i>
-            </div>
-          </div>
-
           <div class="notification-bar-details">
-          <a href="$url" class="notification-bar-title">
-              $searchQuery['text']
+            <a href="<?= $term['url'] ?>" class="notification-bar-title">
+              <!-- <?= $term['url'] ?>  --><?= $term['text'] ?>
             </a>
-            <span class="text-muted">$searchQuery['url']</span>
+            <span class="text-muted"><?= $term['url'] ?></span>
           </div>
         </li>
         <!-- End of single result -->
-        <li class="list-group-item notification-bar-fail m-b-1">
-          <div href="#" class="notification-bar-icon">
-            <div>
-              <i></i>
-            </div>
-          </div>
+        
 
-          <div class="notification-bar-details">
-          <a href="$url" class="notification-bar-title">
-              $searchQuery['text']
-            </a>
-            <span class="text-muted">$searchQuery['url']</span>
-          </div>
-        </li>
-        <!-- End of single result -->
-        <li class="list-group-item notification-bar-fail m-b-1">
-          <div href="#" class="notification-bar-icon">
-            <div>
-              <i></i>
-            </div>
-          </div>
+        <?php
+        endforeach;
+        ?>
 
-          <div class="notification-bar-details">
-          <a href="$url" class="notification-bar-title">
-              $searchQuery['text']
-            </a>
-            <span class="text-muted">$searchQuery['url']</span>
-          </div>
-        </li>
-        <!-- End of single result -->
       </ul>
     </div>
   </div>
@@ -96,6 +59,6 @@ require 'partials/navigation.php';
 
 <?php
 
-  require 'partials/footer-bottom.php';
-  require 'partials/footer.php';
+require 'partials/footer-bottom.php';
+require 'partials/footer.php';
 ?>
