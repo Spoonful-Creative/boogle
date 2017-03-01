ddff74a87a9e0e0bba73f8b91548a88031a982f2
Spoonful-Creative <pughsinead@hotmail.com>
2017-03-02 09:08:15 +1300

    Adding Required Fields

--- feedback.php
+++ feedback.php
@@ -8,10 +8,10 @@
 
 <div style="text-align: center;">
 <form method="POST" action="thanks.php">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+Name: <input type="text" name="name" value="" size="45" required=""><br>
+E-mail: <input type="text" name="email" value="" size="45" required=""><br>
 Feedback: <br>
-<textarea rows="4" cols="50" name="feedback">
+<textarea rows="4" cols="50" name="feedback" required="">
 Enter text here...</textarea><br>
 <input type="submit" /> 
 </form>
